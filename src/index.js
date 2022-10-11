a40e56a3ed90c45beae6b4eccfe20dd7246a06e8
OlenaBielova <belova.elena11@gmail.com>
2022-10-11 23:10:25 +0300

    create gallery

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import {PixabayAPI} from './js/PixabayAPI';
 
 const pixabay = new PixabayAPI();
-console.log(pixabay);
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
@@ -18,12 +17,38 @@ elements: { searchQuery },
     } = e.currentTarget;
 
     const query = searchQuery.value.trim().toLowerCase();
-    
+    console.log(query);
     if (!query) {
         return;
     }
-    console.log('get');
+    pixabay.getImage(query).then(({hits}) => {
+        console.log(hits);
+        const markup = createMarkup(hits);
+        refs.gallery.insertAdjacentHTML('beforeend', markup);
+    });
 };
 
-
 refs.searchForm.addEventListener('submit', onSubmit);
+
+function createMarkup(hits) {
+    return hits.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+        
+        return `<div class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b><br>${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b><br>${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b><br>${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b><br>${downloads}
+            </p>
+          </div>
+        </div>`;
+    }).join('');
+}
--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -1,7 +1,7 @@
 export class PixabayAPI {
-    getImage() {
-        const url = "https://pixabay.com/api/?key=30498969-67220c0dc8cbfcdc961351d72&q=yellow+flowers&image_type=photo";
-        fetch(url).then((response) => {
+    getImage(query) {
+        const url = `https://pixabay.com/api/?key=30498969-67220c0dc8cbfcdc961351d72&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
+        return fetch(url).then((response) => {
             if (!response.ok) {
                 throw new Error(response.status);
             }
